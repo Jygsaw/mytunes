dd0341c68ed2c9fdd607855f50efb36ceee4c0c2
Hack Reactor Students <jygsaw@gmail.com>
2014-06-19 12:17:38 -0700

    Add playCount to LibraryEntryView

--- client/views/LibraryEntryView.js
+++ client/views/LibraryEntryView.js
@@ -3,14 +3,18 @@ var LibraryEntryView = Backbone.View.extend({
 
   tagName: 'tr',
 
-  template: _.template('<td>(<%= artist %>)</td><td><%= title %></td>'),
-
+  template: _.template('<td>(<%= artist %>)</td><td><%= title %></td><td><%= playCount %></td>'),
+  initialize: function(){
+    this.model.on('change:playCount', function(model){
+      this.render();
+    }, this);
+  },
   events: {
     'click': function() {
       // queue songs instead of immediate play
       this.model.enqueue();
       // this.model.play();
-    }
+    },
   },
 
   render: function(){
